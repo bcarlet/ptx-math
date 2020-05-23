79e8191b19c78e72b4ba4a662fd9d5a9f554c7e8
Benjamin-59 <benjamin.david.59@gmail.com>
2020-05-22 19:08:34 -0700

    Add Kahan sum utility

---
+++ model/util/running_stats.hpp
@@ -0,0 +1,50 @@
+#ifndef RUNNING_STATS_HPP
+#define RUNNING_STATS_HPP
+
+#include <algorithm>
+#include <limits>
+
+template<class T>
+class kahan_sum
+{
+public:
+    void accumulate(T val)
+    {
+        const T y = val - c;
+        const T t = sum + y;
+        c = (t - sum) - y;
+        sum = t;
+    }
+
+    T sum = 0;
+
+private:
+    T c = 0;
+};
+
+template<class T, class AvgType = double>
+class running_stats
+{
+public:
+    void accumulate(T val)
+    {
+        count++;
+        min = std::min(min, val);
+        max = std::max(max, val);
+        sum.accumulate(val);
+    }
+
+    AvgType average() const
+    {
+        return (AvgType)sum.sum / count;
+    }
+
+    unsigned long long count = 0u;
+    T min = std::numeric_limits<T>::max();
+    T max = std::numeric_limits<T>::lowest();
+
+private:
+    kahan_sum<T> sum;
+};
+
+#endif
