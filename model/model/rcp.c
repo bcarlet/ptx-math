61bd703def6d60bbd89613949168f10e061d478b
Benjamin-59 <benjamin.david.59@gmail.com>
2020-06-01 12:13:54 -0700

    Explicit argument reduction

--- model/model/rcp.c
+++ model/model/rcp.c
@@ -16,8 +16,22 @@ float model_rcp(float x)
 #ifdef PTX_FTZ
     case FP_SUBNORMAL:
         return copysignf(INFINITY, x);
+#else
+    case FP_SUBNORMAL:
+        return 1.0f / x;    // can't handle subnormals explicitly yet
 #endif
     }
 
-    return 1.0f / x;
+    // x = fraction * 2^exp
+    // fraction in (-1, -0.5] U [0.5, 1)
+    int exp;
+    float fraction = frexpf(x, &exp);
+
+    // scale fraction to (-2, -1] U [1, 2)
+    exp -= 1;
+    fraction *= 2;
+
+    float r = 1.0f / fraction;
+
+    return ldexpf(r, -exp);
 }
