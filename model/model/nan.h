ce406c82987348482fdef6fa4f920334084d2cdb
Benjamin-59 <benjamin.david.59@gmail.com>
2020-06-03 17:06:31 -0700

    Use common fpmanip functions

--- model/model/nan.h
+++ model/model/nan.h
@@ -1,17 +1,13 @@
 #ifndef NAN_H
 #define NAN_H
 
-#include <stdint.h>
-#include <string.h>
+#include "fpmanip.h"
+
+#define PTX_NAN UINT32_C(0x7fffffff)
 
 inline float canonical_nan(void)
 {
-    const uint32_t inan = UINT32_C(0x7fffffff);
-
-    float fnan;
-    memcpy(&fnan, &inan, 4u);
-
-    return fnan;
+    return reinterpret_uint(PTX_NAN);
 }
 
 #endif
