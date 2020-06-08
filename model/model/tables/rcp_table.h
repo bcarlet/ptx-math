14e6897b74fe3228779be97823fa6093a4ed7104
Benjamin-59 <benjamin.david.59@gmail.com>
2020-06-07 18:37:58 -0700

    Refactor tables

---
+++ model/model/tables/rcp_table.h
@@ -0,0 +1,13 @@
+#ifndef RCP_TABLE_H
+#define RCP_TABLE_H
+
+#include <stdint.h>
+
+#define RCP_M 7
+#define RCP_T 26
+#define RCP_P 16
+#define RCP_Q 10
+
+extern const uint32_t rcp_table[128][3];
+
+#endif
