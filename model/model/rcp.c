d74e4f55e0c6c32453ef227745a56ce2c2a2f811
Benjamin-59 <benjamin.david.59@gmail.com>
2020-06-17 11:55:03 -0700

    Parameterize rcp

--- model/model/rcp.c
+++ model/model/rcp.c
@@ -1,11 +1,25 @@
 #include "model.h"
+#include "tuning.h"
+
 #include "tables/rcp_table.h"
 #include "common/fpmanip.h"
 #include "common/nan.h"
+#include "common/squarer.h"
 
 #include <math.h>
 
+static const m_params model_params =
+{
+    .table = rcp_table,
+    .bias = UINT64_C(0x8000000000000000)
+};
+
 float model_rcp(float x)
+{
+    return parameterized_rcp(x, &model_params);
+}
+
+float parameterized_rcp(float x, const m_params *params)
 {
     switch (fpclassify(x))
     {
@@ -29,30 +43,32 @@ float model_rcp(float x)
     const uint32_t xh = UPPER_SIGNIFICAND(x_bits, RCP_M);
     const uint32_t xl = LOWER_SIGNIFICAND(x_bits, RCP_M);
 
-    const uint32_t *const c = rcp_table[xh];
+    const uint32_t *const c = params->table[xh];
 
-    const uint32_t c1_term = c[1] * xl;
-    const uint64_t c2_term = (uint64_t)c[2] * (xl * xl);
+    uint64_t c0_term = c[0];
+    uint64_t c1_term = c[1] * xl;   // won't exceed 32 bits
+    uint64_t c2_term = c[2] * square_approx(xl);
 
-    const uint64_t c0_aligned = (uint64_t)c[0] << 30;
-    const uint64_t c1_term_aligned = (uint64_t)c1_term << 17;
+    c0_term <<= RCP_C0_TERM_ALIGNMENT;
+    c1_term <<= RCP_C1_TERM_ALIGNMENT;
+    c2_term <<= RCP_C2_TERM_ALIGNMENT;
 
-    uint64_t sum = c0_aligned - c1_term_aligned + c2_term;
+    uint64_t sum = c0_term - c1_term + c2_term;
 
-    sum += UINT64_C(1) << (56 - 24);
+    sum += params->bias >> ((64 - RCP_SUM_WEIGHT) + 23);
 
     uint32_t r_exp = 127u;
 
-    if (!(sum >> 56))
+    if (!(sum >> RCP_SUM_WEIGHT))
     {
         r_exp -= 1;
         sum <<= 1;
     }
 
-    const uint32_t r_sig = (sum >> (56 - 23)) & MASK_U32(23);
+    const uint32_t r_frac = (sum >> (RCP_SUM_WEIGHT - 23)) & MASK_U32(23);
     const uint32_t r_sign = signbit(x) ? 1u : 0u;
 
-    const uint32_t r_bits = FP_FORMAT(r_sign, r_exp, r_sig);
+    const uint32_t r_bits = FP_FORMAT(r_sign, r_exp, r_frac);
 
     const float r = reinterpret_uint(r_bits);
 
--- model/model/tables/rcp_table.h
+++ model/model/tables/rcp_table.h
@@ -8,6 +8,12 @@
 #define RCP_P 16
 #define RCP_Q 10
 
+#define RCP_C0_TERM_ALIGNMENT 30
+#define RCP_C1_TERM_ALIGNMENT 17
+#define RCP_C2_TERM_ALIGNMENT 0
+
+#define RCP_SUM_WEIGHT 56
+
 extern const uint32_t rcp_table[128][3];
 
 #endif
