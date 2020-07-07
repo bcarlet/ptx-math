2d0baf9f84f1076d94d7132236eb442c552d4592
Benjamin-59 <benjamin.david.59@gmail.com>
2020-07-07 16:21:54 -0700

    Fix missing include; add 64-bit mask

--- model/model/common/bitmask.h
+++ model/model/common/bitmask.h
@@ -1,6 +1,9 @@
 #ifndef BITMASK_H
 #define BITMASK_H
 
+#include <stdint.h>
+
 #define MASK_U32(numbits) ((UINT32_C(1) << (numbits)) - 1u)
+#define MASK_U64(numbits) ((UINT64_C(1) << (numbits)) - 1u)
 
 #endif
